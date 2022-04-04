072ff78786f4167d1fa493940b64cdf5ce714226
Michael Bora Sanuk <bora@aeontech.io>
2022-04-04 23:21:21 +0300

    added item list

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 const App = (): JSX.Element => {
+  const [items, setItems] = useState<string[]>([]);
+  const addItem = (name: string): void => {
+    setItems([...items, name]);
+  };
+  const createItemTile = (name: string): JSX.Element => {
+    return <div className="item-tile">{name}</div>;
+  };
+
   return (
-  <>
-  <div className="item-list">
-    <div className="item-tile">
-      Eiffel Tower
-    </div>
-    <div className="item-tile">
-      Eiffel Tower
-    </div>
-    <div className="item-tile">
-      Eiffel Tower
-    </div>
-  </div>
-  <Button variant="primary">Add Item</Button>
-  </>
+    <>
+      <div className="item-list">
+        {items.map(e => createItemTile(e))}
+      </div>
+      <div className="py-1"></div>
+      <Button variant="primary" onClick={() => addItem("Eiffel Tower")}>
+        Add Item
+      </Button>
+    </>
   );
 };
 
