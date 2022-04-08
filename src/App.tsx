57fc76afb541f9a659950d5c3c602707a794b60b
Michael Bora Sanuk <bora@aeontech.io>
2022-04-08 03:09:12 +0300

    added suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import "./App.css";
-import Button from "react-bootstrap/Button";
 import Item, { ItemData } from "components/item/Item";
+import { getRandomItem, getRandomSuggestions } from "services/TripService";
 
 const App = (): JSX.Element => {
   const [items, setItems] = useState<ItemData[]>([]);
-  const addItem = (name: string): void => {
-    setItems([...items, { name: name, address: "Paris" }]);
+  const addItem = (itemData: ItemData): void => {
+    setItems([...items, itemData]);
+  };
+
+  const [choices, setChoices] = useState<ItemData[]>([
+    { name: "Loading", address: "..." },
+  ]);
+
+  const updateChoices = (): void => {
+    getRandomSuggestions().then((e) => setChoices(e));
+  };
+
+  useEffect(() => {
+    console.log("trigger");
+    updateChoices();
+  }, []);
+
+  const showChoices = () => {
+    return (
+      <div className="suggestion-container">
+        {choices.map((e) => (
+          <div
+            className="suggestion-tile"
+            onClick={() => {
+              addItem(e);
+              setChoices([]);
+              updateChoices();
+            }}
+          >
+            <div className="item-title">{e.name}</div>
+            <div className="item-desc">{e.address}</div>
+          </div>
+        ))}
+      </div>
+    );
   };
 
   return (
     <>
       <div className="container py-3">
-        <Button
-          variant="primary"
-          onClick={() => {
-            addItem("Eiffel Tower");
-          }}
-        >
-          Add Item
-        </Button>
         <div className="item-list">
           {items.map((e) => (
             <Item itemData={e} />
           ))}
         </div>
         <div className="py-1"></div>
+        {showChoices()}
       </div>
     </>
   );
--- src/services/TripService.ts
+++ src/services/TripService.ts
@@ -1,8 +1,15 @@
-import React from "react";
+import { ItemData } from "components/item/Item";
 
-export const getRandomItem = async (): Promise<string> => {
+export const getRandomItem = async (): Promise<ItemData> => {
   const response = await fetch("http://127.0.0.1:5000/randomitem");
   const data = await response.json();
-  console.log(data); 
-  return "done";
+  console.log(data);
+  return data;
+};
+
+export const getRandomSuggestions = async (): Promise<ItemData[]> => {
+  const response = await fetch("http://127.0.0.1:5000/randomsuggestions");
+  const data = await response.json();
+  console.log(data);
+  return data;
 };
